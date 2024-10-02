5b172f459d4e150d2108ddd77756bdbf4654d47e
hongbocheng <117956865+hongbocheng@users.noreply.github.com>
2024-10-02 16:36:06 +0800

    Create base36.sh
    add a script to do base36 to base 10 conversion and vice versa

---
+++ base36.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## base36 converstion taken from 
+## https://boulderappsco.postach.io/post/convert-decimal-to-base-36-alpha-numeric-in-bash-linux
+function decimal_to_base36() {
+        BASE36=($(echo {0..9} {A..Z}));
+        arg1=$@;
+        for i in $(bc <<< "obase=36; $arg1"); do
+echo -n ${BASE36[$(( 10#$i ))]}
+done && echo
+}
+
+if [ $1 == "-r" ]; then
+        shift
+        for i in "$@"
+        do
+                echo -n "$i ... ";bc<<<"ibase=36;$i"
+        done
+else
+
+        for i in "$@"
+        do
+                echo -n "$i --- ";decimal_to_base36 $i
+        done
+fi
